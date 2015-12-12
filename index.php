425253333d5e15e0bd72763de61bea0e34b9df24
Sirvan Almasi <sirvan3tr@gmail.com>
2015-12-12 14:15:22 +0000

    Basic Structure

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+include('views/notloggedin.php');
+?>
---
+++ views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- Bootstrap -->
+    <link href="static/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="static/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <link href="static/css/style.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Sree+Krushnadevaraya' rel='stylesheet' type='text/css'>
+
+    <title>SportsBuddy</title>
+  </head>
+  <body>
+<h2>Welcome</h2>
+  </body>
+</html>
