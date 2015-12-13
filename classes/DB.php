398775bfba545f478040e1c1576e6c9531fd08fa
Neitsch <nigel.schusters@googlemail.com>
2015-12-13 07:21:03 +0000

    Ommitting own events

--- classes/DB.php
+++ classes/DB.php
@@ -12,4 +12,25 @@ class DB{
             return self::$db;
         }
     }
+
+    public static function getMyEvents($limit) {
+	$d = self::mongoDB();
+        if(isset($_SESSION['fb_user_id'])) {
+	    $u = $d->sports->users->findOne(array("id" => $_SESSION['fb_user_id']));
+	    return $d->sports->events->find(array("_id" => array('$in' => $u['events'])))->limit($limit);
+	}
+        return array();
+    }
+
+    public static function getEvents($limit) {
+	$d = self::mongoDB();
+        if(isset($_SESSION['fb_user_id'])) {
+            $u = $d->sports->users->findOne(array("id" => $_SESSION['fb_user_id']));
+	    return $d->sports->events->find(array("_id" => array('$nin' => $u['events']), "user_id" => array('$ne' => $u['id'])))->sort(array("id" => 1))->limit($limit);
+	}
+        else {
+	    return $d->sports->events->find()->sort(array("id" => 1))->limit($limit);
+        }
+    }
 }
+?>
