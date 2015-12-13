291171430d80459a0ac59794518da5ade31f13ae
Neitsch <nigel.schusters@googlemail.com>
2015-12-13 00:00:08 +0000

    Disabled textmessage for now

--- accept.php
+++ accept.php
@@ -1,11 +1,17 @@
 <?php
+  session_start();
   require_once __DIR__ . '/vendor/autoload.php';
   $id = $_GET['id'];
+  $m = new MongoClient();
+  $user = $m->sports->users->findOne(array("id" => $_SESSION['fb_user_id']));
+  $ev = $m->sports->events->findOne(array("_id" => new MongoId($id)));
+  $sport = $m->sports->sport->findOne(array("internal" => $ev['sport']));
   $MessageBird = new \MessageBird\Client('test_FwajaLoWbqkp5ncnbt9sfBLH6');
   $Message = new \MessageBird\Objects\Message();
   $Message->originator = 'MessageBird';
   $Message->recipients = array("+4907913650254");
-  $Message->body = 'Somebody accepted the event with ID: '.$id.'.';
 
-  $MessageBird->messages->create($Message);
+  $Message->body = $user['name'].' wants to play '.$sport['name'].' with you.';
+  echo $Message->body;
+  //$MessageBird->messages->create($Message);
 ?>
