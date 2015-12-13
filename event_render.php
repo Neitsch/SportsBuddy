272e022a13cde482ab8f39f3bbd9320889a29ed2
Sirvan Almasi <sirvan3tr@gmail.com>
2015-12-13 09:33:40 +0000

    list styling

--- event_render.php
+++ event_render.php
@@ -5,20 +5,21 @@
                 if($event['user_id'] != $_SESSION['fb_user_id']) {
 		  $val .= "<a href='payment.php?id=".((string)$event['_id'])."'>";
 		}
-		$val .= "<img class='img-rounded' src=\"http://graph.facebook.com/v2.5/".$event['user_id']."/picture\"></img>";
+		$val .= "<img class='facebook-profilepic img-rounded' src=\"http://graph.facebook.com/v2.5/".$event['user_id']."/picture\"></img>";
 		$val .= $m->sports->users->findOne(array("id" => $event['user_id']))['name'];
-		$val .= " is looking for ".$event['maxpeople']." people";
+		$val .= " is looking for ".$event['maxpeople'];
 		$val .= $m->sports->sport->findOne(array("internal" => $event['sport']))['name'];
   		$val .= " buddy on ";
-		$val .= $event['sessiondate'];
+		$val .= '<strong>'.$event['sessiondate'].'</strong>';
   		$val .= " at ";
-		$val .= $event['sessiontime'];
-		$val .= " for ";
-		$val .= $event['eventfee']."£";
+		$val .= '<strong>'.$event['sessiontime'].'</strong>';
+		if($event['eventfee'] > 0) {
+			$val .= " for &pound;".$event['eventfee'];
+		}
 		if($event['user_id'] != $_SESSION['fb_user_id']) {
 			$val .= "</a>";
 		}
-		$val .= "</div>";
+		$val .= "<div class='clear'></div></div>";
 		return $val;
 	}
 ?>
