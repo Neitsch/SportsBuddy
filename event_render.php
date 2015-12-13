afc49fe696d27a9f10b53438464e6b8c58a151ff
Neitsch <nigel.schusters@googlemail.com>
2015-12-13 00:24:01 +0000

    Added profile pic

--- event_render.php
+++ event_render.php
@@ -2,7 +2,7 @@
 	function render_event ($event) {
 		$m = new MongoClient();
 		require_once __DIR__ . '/vendor/autoload.php';
-		$val = "<li><a href='accept.php?id=".((string)$event['_id'])."'>" . $m->sports->sport->findOne(array("internal" => $event['sport']))['name'];
+		$val = "<li><a href='accept.php?id=".((string)$event['_id'])."'><img src=\"http://graph.facebook.com/v2.5/".$event['user_id']."/picture\"></img>" . $m->sports->sport->findOne(array("internal" => $event['sport']))['name'];
   		$val .= " by ".$m->sports->users->findOne(array("id" => $event['user_id']))['name'];
   		$val .= "</a></li>";
 		return $val;
